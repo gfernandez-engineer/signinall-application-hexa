29fbfa70a395f7d2ee9353f72ff89d348375c1a8
Giancarlo Fernandez <giancarlo.fernandez@outlook.com>
2024-11-20 00:49:15 -0500

    estructurando el proyecto con el modelo hecagonal

---
+++ signinall-application-hexa/application/src/main/java/com/signinall/signinall_application_hexa/application/ApplicationLauncher.java
@@ -0,0 +1,12 @@
+package com.signinall.signinall_application_hexa.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationLauncher {
+    public static void main(String[] args){
+        SpringApplication.run(ApplicationLauncher.class,args);
+    }
+
+}
---
+++ signinall-application-hexa/infraestructure/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.signinall;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
