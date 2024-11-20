333af25eb2dd02e7e87ad5df022ac9020a5a211f
Giancarlo Fernandez <giancarlo.fernandez@outlook.com>
2024-11-19 21:12:45 -0500

    adding new project

---
+++ signinall-application-hexa/src/main/java/com/signinall/signinall_application_hexa/SigninallApplicationHexaApplication.java
@@ -0,0 +1,13 @@
+package com.signinall.signinall_application_hexa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigninallApplicationHexaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigninallApplicationHexaApplication.class, args);
+	}
+
+}
---
+++ signinall-application-hexa/src/test/java/com/signinall/signinall_application_hexa/SigninallApplicationHexaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.signinall.signinall_application_hexa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigninallApplicationHexaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
